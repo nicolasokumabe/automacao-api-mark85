8be93f8e3801ba3ea7b70864ccf59f4365f840f4
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-03 19:11:16 -0300

    POST request

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('POST /users', ()=> {
+
+  it('register a new user', ()=> {
+
+    const user = {
+      name: 'Nicolas Kumabe',
+      email: 'nicolas@yahoo.com',
+      password: 'pwd123'
+    }
+    
+    cy.request({
+      url: '/users',
+      method: 'POST',
+      body: user,
+      failOnStatusCode: false
+    }).then(response => {
+      expect(response.status).to.eq(200)
+    })
+
   })
+
+
 })
