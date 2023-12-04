67fa7e0568db238697114f8d8abbdb54596b24a0
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-04 18:03:02 -0300

    Controlando Entrada e Saida de Dados

--- cypress/e2e/sessions.cy.js
+++ cypress/e2e/sessions.cy.js
@@ -2,17 +2,20 @@ describe('POST /sessions', () => {
 
     it('user session', () => {
         const userData = {
-            name: 'Nicolas Kumabe',
-            email: 'nicolas@yahoo.com',
+            name: 'Will Smith',
+            email: 'slap@hotmail.com',
             password: 'pwd123'
         }
 
+        cy.task('deleteUser', userData.email)
+        cy.postUser(userData)
+
         cy.postSession(userData)
             .then(response => {
+                expect(response.status).to.eq(200)
 
                 const { user, token } = response.body
 
-                expect(response.status).to.eq(200)
                 expect(user.name).to.eq(userData.name)
                 expect(user.email).to.eq(userData.email)
                 expect(token).not.to.be.empty
