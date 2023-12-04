1fdc1b7020399ae0635070b776cdfae6c6ab0411
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-04 16:13:57 -0300

    Aprimorando Cobertura de Testes

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,6 +1,6 @@
 describe('POST /users', () => {
 
-  it('register a new user', () => {
+  it('duplicate email', () => {
 
     const user = {
       name: 'Nicolas Kumabe',
@@ -13,6 +13,28 @@ describe('POST /users', () => {
     cy.postUser(user)
       .then(response => {
         expect(response.status).to.eq(200)
+      })
+  })
+
+  it('register a new user', () => {
+
+    const user = {
+      name: 'Ramon Dino',
+      email: 'dino@hotmail.com',
+      password: 'pwd123'
+    }
+
+    cy.task('deleteUser', user.email)
+
+    cy.postUser(user)
+
+    cy.postUser(user)
+      .then(response => {
+
+        const { message } = response.body
+
+        expect(response.status).to.eq(409)
+        expect(message).to.eq('Duplicated email!')
       })
   })
 })
