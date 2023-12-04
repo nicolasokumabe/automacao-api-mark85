133e5fdd6bb4ec39e51e924e0f86f258d7c8c494
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-04 14:55:23 -0300

    Adicionado Custom Commands

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,27 +1,19 @@
-describe('POST /users', ()=> {
+describe('POST /users', () => {
 
-  it('register a new user', ()=> {
+  it('register a new user', () => {
 
     const user = {
       name: 'Nicolas Kumabe',
       email: 'nicolas@yahoo.com',
       password: 'pwd123'
     }
-    
+
     cy.task('deleteUser', user.email)
 
-
-    cy.api({
-      url: '/users',
-      method: 'POST',
-      body: user,
-      failOnStatusCode: false
-    }).then(response => {
-      expect(response.status).to.eq(200)
-      cy.log(JSON.stringify(response.body))
-    })
-
+    cy.postUser(user)
+      .then(response => {
+        expect(response.status).to.eq(200)
+      })
   })
-
-
 })
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('postUser', (user) => {
+    cy.api({
+        url: '/users',
+        method: 'POST',
+        body: user,
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
