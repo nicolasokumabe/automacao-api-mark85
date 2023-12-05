6fa15c25922a2467896df513f18db5b40b61fb2e
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-05 11:01:50 -0300

    Implementando Fixtures & Funcoes do Javascript

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,3 +32,12 @@ Cypress.Commands.add('postUser', (user) => {
         failOnStatusCode: false
     }).then(response => { return response })
 })
+
+Cypress.Commands.add('postSession', (user) => {
+    cy.api({
+        url: '/sessions',
+        method: 'POST',
+        body: { email: user.email, password: user.password },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
