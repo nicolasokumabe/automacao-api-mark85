c0b0fedad67f1241b937a1e042752580a1aab4f8
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-06 15:08:53 -0300

    Validando response body

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -13,12 +13,17 @@ describe('POST /tasks', () => {
         cy.postUser(user)
 
         cy.postSession(user)
-            .then(response => {
+            .then(userResp => {
                 cy.task('deleteTask', task.name, user.email)
 
-                cy.postTask(task, response.body.token)
+                cy.postTask(task, userResp.body.token)
                     .then(response => {
                         expect(response.status).to.eq(200)
+                        expect(response.body.name).to.eq(task.name)
+                        expect(response.body.tags).to.eql(task.tags)
+                        expect(response.body.is_done).to.be.false
+                        expect(response.body.user).to.eq(userResp.body.user._id)
+                        expect(response.body._id.length).to.eq(24)
                     })
             })
     })
