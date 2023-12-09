3e1491c48c7cfbcfbe0faef9c55cd214048cd655
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-09 15:23:37 -0300

    Ajuste na automacao com novos status code

--- cypress/e2e/tasks/post.cy.js
+++ cypress/e2e/tasks/post.cy.js
@@ -18,7 +18,7 @@ describe('POST /tasks', () => {
 
                 cy.postTask(task, userResp.body.token)
                     .then(response => {
-                        expect(response.status).to.eq(200)
+                        expect(response.status).to.eq(201)
                         expect(response.body.name).to.eq(task.name)
                         expect(response.body.tags).to.eql(task.tags)
                         expect(response.body.is_done).to.be.false
